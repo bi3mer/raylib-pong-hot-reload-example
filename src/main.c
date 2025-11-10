74ae7703add0909688f799b1e119df6111c52d3a
bi3mer <colan@comcast.net>
2025-11-10 16:35:29 -0500

    render player score and added basic state info

--- src/main.c
+++ src/main.c
@@ -1,19 +1,58 @@
 #include "raylib.h"
+#include <stdio.h>
 
 int main(void)
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    ///////////////////////////////////////////////////////////////////////////
+    // Game State
+    int player_left_score = 0;
+    int player_right_score = 0;
 
-    InitWindow(screenWidth, screenHeight, "Raylib Template");
+    Rectangle paddle_left = {
+        .x = 0.1f,
+        .y = 0.5f,
+        .width = 0.05f,
+        .height = 0.2f,
+    };
+
+    Rectangle paddle_right = {
+        .x = 0.9f,
+        .y = 0.5f,
+        .width = 0.05f,
+        .height = 0.2f,
+    };
+
+    float ball_radius = 0.1f;
+    Vector2 ball_position = {
+        .x = 0.5f,
+        .y = 0.5f,
+    };
+    Vector2 ball_velocity = {
+        .x = 1.0f,
+        .y = 0.0f,
+    };
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Raylib
+    const int width = 1080;
+    const int height = 720;
+
+    InitWindow(width, height, "Raylib Template");
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
 
-        DrawText("TEXT HERE", 190, 200, 20, BLACK);
+        char buffer[4];
+        sprintf(buffer, "%d", player_left_score);
+        DrawText(buffer, 100, 20, 20, WHITE);
+
+        sprintf(buffer, "%d", player_right_score);
+        DrawText(buffer, 980, 20, 20, WHITE);
+
+        DrawText("Pong", width / 2, 20, 20, WHITE);
 
         EndDrawing();
     }
